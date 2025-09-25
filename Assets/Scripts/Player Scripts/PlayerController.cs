ce440488d5116b1d05e1fb70fe9bf9c0afddb358
Raammmaaaannnnn <99628801+Raammmaaaannnnn@users.noreply.github.com>
2025-09-25 11:54:06 +1000

    Road Layout and Player Movement Script
    Road Laid out for the town along with collision grid. Testing player movement against collision.

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+
+    Vector2 movementInput;
+    Rigidbody2D rb;
+
+    List<RaycastHit2D> castCollision = new List<RaycastHit2D>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        if (movementInput != Vector2.zero) 
+        {
+            bool success = TryToMove(movementInput);
+
+            if(!success)
+            {
+                success = TryToMove(new Vector2(movementInput.x, 0));
+                if (!success)
+                {
+                    success = TryToMove(new Vector2(0, movementInput.y));
+                }
+            }
+        }
+    }
+
+    private bool TryToMove(Vector2 direction)
+    {
+
+        int count = rb.Cast(
+            direction,
+            movementFilter,
+            castCollision,
+            moveSpeed * Time.fixedDeltaTime * collisionOffset
+
+        );
+
+        if (count == 0)
+        {
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            Debug.Log("Moving");
+            return true;
+        }
+        else{
+            Debug.Log("NotMoving");
+            return false;
+        }
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
     }
 }
