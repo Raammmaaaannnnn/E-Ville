373e7b64cddf3a0122156d3d11632dfb0518adf5
Raammmaaaannnnn <99628801+Raammmaaaannnnn@users.noreply.github.com>
2025-10-30 15:20:00 +1100

    Playermovement Changes

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -6,6 +6,56 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Animator animator;
+    public _Attack attack_;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = moveInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        animator.SetBool("isMoving?", true);
+
+        if(context.canceled)
+        {
+            animator.SetBool("isMoving?", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+        moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputXCurrent", moveInput.x);
+        animator.SetFloat("InputYCurrent", moveInput.y);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /////////////////////////////
+
+    /*
+
     public float moveSpeed = 1f;
 
     public float collisionOffset = 0.05f;
@@ -184,5 +234,7 @@ public class PlayerController : MonoBehaviour
     {
         canMove = true;
     }
+
+    */
 
 }
