9abde23a33600b31b03f21cb1effac7afae34da4
Raammmaaaannnnn <99628801+Raammmaaaannnnn@users.noreply.github.com>
2025-10-31 04:59:16 +1100

    changes need to work on attacking

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,13 +8,30 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] _Attack attack_Hitbox;
+    [Header("Attack Boxes")]
+    [SerializeField]
+    GameObject attackHitBoxLeftAndRight;
+    [SerializeField]
+    GameObject attackHitBoxDown;
+    [SerializeField]
+    GameObject attackHitBoxUp;
+
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private Animator animator;
-    public _Attack attack_;
+    private bool canMove = true;
+    private bool isAttacking = false;
     // Start is called before the first frame update
     void Start()
     {
+        GameObject attackHitBoxUp = GetComponent<GameObject>();
+        attackHitBoxUp.SetActive(false);
+        GameObject attackHitBoxDown = GetComponent<GameObject>();
+        attackHitBoxDown.SetActive(false);
+        GameObject attackHitBoxLeftAndRight = GetComponent<GameObject>();
+        attackHitBoxLeftAndRight.SetActive(false);
+
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
@@ -21,23 +39,118 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = moveInput * moveSpeed;
-    }
+        if (canMove) // Stop movement when attacking
+        {
+            rb.velocity = moveInput * moveSpeed;
+        }
+        else
+        {
 
+            rb.velocity = Vector2.zero;
+            
+        }
+        //rb.velocity = moveInput * moveSpeed;
+    }
+   
     public void Move(InputAction.CallbackContext context)
     {
-        animator.SetBool("isMoving?", true);
+        //if (isAttacking) return; // Ignore movement input during attack
 
-        if(context.canceled)
+        //canMove = true;
+        //animator.SetBool("isMoving?", true);
+
+        //if (moveInput != Vector2.zero)//context.canceled)
+        //{
+        //    animator.SetBool("isMoving?", false);
+        //    //if (isAttacking) return;
+        //    animator.SetFloat("LastInputX", moveInput.x);
+        //    animator.SetFloat("LastInputY", moveInput.y);
+        //}
+        //moveInput = context.ReadValue<Vector2>();
+        //animator.SetFloat("InputXCurrent", moveInput.x);
+        //animator.SetFloat("InputYCurrent", moveInput.y);
+
+        moveInput = context.ReadValue<Vector2>();
+
+        if (!canMove)
+            moveInput = Vector2.zero;
+
+        // Update blend tree parameters
+        animator.SetFloat("InputXCurrent", moveInput.x);
+        animator.SetFloat("InputYCurrent", moveInput.y);
+
+        // Determine if player is moving
+        bool moving = moveInput.magnitude > 0.1f;
+        animator.SetBool("isMoving?", moving);
+
+        // Update last input for idle direction when moving
+        if (moving)
         {
-            animator.SetBool("isMoving?", false);
             animator.SetFloat("LastInputX", moveInput.x);
             animator.SetFloat("LastInputY", moveInput.y);
         }
-        moveInput = context.ReadValue<Vector2>();
-        animator.SetFloat("InputXCurrent", moveInput.x);
-        animator.SetFloat("InputYCurrent", moveInput.y);
     }
+
+    public void OnAttackInput(InputAction.CallbackContext context)
+    {
+        if (context.performed && !isAttacking)
+        {
+            isAttacking = true;
+            canMove = false;
+            animator.SetTrigger("Attack");
+
+            float x = animator.GetFloat("LastInputX");
+            float y = animator.GetFloat("LastInputY");
+
+            // Direction-based attack handling
+            if (Mathf.Abs(x) > Mathf.Abs(y))
+            {
+                attackHitBoxLeftAndRight.SetActive(true);
+                if (x > 0)
+                    attack_Hitbox.AttackRight();
+                else
+                    attack_Hitbox.AttackLeft();
+            }
+            else
+            {
+                if (y > 0)
+                {
+                    attackHitBoxUp.SetActive(true);
+                    attack_Hitbox.AttackUp();
+                }
+                else
+                {
+                    attackHitBoxDown.SetActive(true);
+                    attack_Hitbox.AttackDown();
+                }
+                 
+            }
+        }
+
+        
+        /*
+        if (context.performed && !isAttacking)
+        {
+            isAttacking = true;
+            animator.SetTrigger("Attack");
+
+            // Determine direction of attack
+            if (animator.GetFloat("LastInputX") < 0)
+                attack_Hitbox.AttackLeft();
+            else
+                attack_Hitbox.AttackRight();
+
+        }*/
+    }
+
+    // Called from Animation Event
+    public void StopAttack()
+    {
+        attack_Hitbox.StopAttack();
+        isAttacking = false;
+        canMove = true;
+    }
+
 
 
 
--- Assets/_Attack.cs
+++ Assets/_Attack.cs
@@ -4,30 +4,53 @@ using UnityEngine;
 
 public class _Attack : MonoBehaviour
 {
-    public float damage = 5;
-
+    [SerializeField] private float damage = 5;
     public Collider2D Attackbox;
-
-    Vector2 attackOffsetright;
+    private Vector2 defaultPos;
 
     private void Start()
     {
-        //Attackbox = GetComponent<Collider2D>();
-        attackOffsetright = transform.localPosition;
+        
+        defaultPos = transform.localPosition;
+        Attackbox.enabled = false;
+        
+
+        //if (Attackbox == null)
+        //    Attackbox = GetComponent<Collider2D>();
+
+        //Attackbox.enabled = false;
+        //defaultPos = transform.localPosition;
     }
+
+
+    public void AttackUp()
+    {
+        Attackbox.enabled = true;
+        transform.localPosition = defaultPos;//offsetUp;
+        Debug.Log("Attacking Up");
+    }
+
+    public void AttackDown()
+    {
+        Attackbox.enabled = true;
+        transform.localPosition = defaultPos;//offsetDown;
+        Debug.Log("Attacking Down");
+       
+    }
+
 
     public void AttackLeft()
     {
         Debug.Log("Attacking Left");
         Attackbox.enabled = true;
-        transform.localPosition = new Vector3(attackOffsetright.x * -1, attackOffsetright.y);
+        transform.localPosition = new Vector3(defaultPos.x * -1, defaultPos.y);
     }
 
     public void AttackRight()
     {
         Debug.Log("Attacking Right");
         Attackbox.enabled = true;
-        transform.localPosition = attackOffsetright; 
+        transform.localPosition = defaultPos; 
 
     }
 
@@ -35,6 +58,8 @@ public class _Attack : MonoBehaviour
     {
         Attackbox.enabled=false;    
     }
+
+    
 
     private void OnTriggerEnter2D(Collider2D other)
     {
